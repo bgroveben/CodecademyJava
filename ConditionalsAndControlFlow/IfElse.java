c162ca6a291c6693c6d5ae09342f1e2bec2eea31
bgrove1974 <bgroveben@gmail.com>
2016-03-21 03:34:10 -0700

    If-ElseIf-Else Statement

---
+++ ConditionalsAndControlFlow/IfElse.java
@@ -0,0 +1,15 @@
+public class IfElse {
+    public static void main(String[] args) {
+        if (1 < 3 && 5 < 4) {
+            System.out.println("I defy the Boolean laws!");
+        } else {
+            System.out.println("You can thank George Boole!");
+        }
+        // And another for good measure
+        if (7 < 7) {
+            System.out.println("Try again...");
+        } else {
+            System.out.println("Success!");
+        }
+    }
+}
