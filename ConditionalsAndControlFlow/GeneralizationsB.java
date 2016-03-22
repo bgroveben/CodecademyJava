8ab3ef702461777ee2ed6f823c1196371a823d08
bgrove1974 <bgroveben@gmail.com>
2016-03-22 03:00:01 -0700

    Add missing line in GeneralizationsB Code

--- ConditionalsAndControlFlow/GeneralizationsB.java
+++ ConditionalsAndControlFlow/GeneralizationsB.java
@@ -13,11 +13,13 @@ public class GeneralizationsB {
         int subwayTrain = 5;
 
         switch (subwayTrain) {
-            case 1: System.out.println("This is a South Ferry bound train!");
-                break;
-            case 5: System.out.println("This is a Queens bound train!");
-                break;
-            default: System.out.println("I'm not sure where that train goes...");
+            case 1 : System.out.println("This is a South Ferry bound train!");
+			         break;
+			case 5 : System.out.println("This is a Brooklyn bound train!");
+			         break;
+			case 7 : System.out.println("This is a Queens bound train!");
+			         break;
+			default: System.out.println("I'm not sure where that train goes...");
         }
     }
 }
