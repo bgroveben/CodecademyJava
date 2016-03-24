80edaeeb658e603a6ae2e6efcf0eff896b36ea1f
bgrove1974 <bgroveben@gmail.com>
2016-03-23 22:09:03 -0700

    Object-Oriented Overview

---
+++ ObjectOrientedJava/Rodentia.java
@@ -0,0 +1,7 @@
+public class Rodentia {
+    public static void main(String[] args) {
+    }
+    public void order() {
+        System.out.println("This animal belongs to the Rodentia order.");
+    }
+}
