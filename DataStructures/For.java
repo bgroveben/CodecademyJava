8e2346270e97dc322b40a70be19c4df77e84c12e
bgrove1974 <bgroveben@gmail.com>
2016-03-31 14:56:24 -0700

    For Loop

---
+++ DataStructures/For.java
@@ -0,0 +1,8 @@
+// A simple for loop
+public class For {
+    public static void main(String[] args) {
+        for (int waterLevel = 0; waterLevel < 7; waterLevel++) {
+            System.out.println("The pool's water level is at " + waterLevel + " feet.");
+        }
+    }
+}
